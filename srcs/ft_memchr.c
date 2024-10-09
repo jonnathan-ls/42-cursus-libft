fffb6ace49664701fa95d11f34d98ca5bba37c11
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2024-10-08 22:09:09 -0300

    feat: adds implementation for memchr function

---
+++ srcs/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 19:56:49 by jlacerda          #+#    #+#             */
+/*   Updated: 2024/10/08 20:32:21 by jlacerda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memchr(const void *s, int c, unsigned long n)
+{
+	unsigned long	i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)s;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char)c)
+			return ((void *)&ptr[i]);
+		i++;
+	}
+	return (0);
+}
