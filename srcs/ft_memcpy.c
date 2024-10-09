92e57b3e6b8b947c6b987fd5316891064f9e7356
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2024-10-08 22:09:17 -0300

    feat: adds implementation for memcpy function

---
+++ srcs/ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 19:56:49 by jlacerda          #+#    #+#             */
+/*   Updated: 2024/10/08 20:46:33 by jlacerda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void *ft_memcpy(void *dest, const void *src, unsigned long n)
+{
+	unsigned long	i;
+	char			*ptr_dest;
+	char			*ptr_src;
+
+	i = 0;
+	ptr_dest = (char *)dest;
+	ptr_src = (char *)src;
+	while (i < n)
+	{
+		ptr_dest[i] = ptr_src[i];
+		i++;
+	}
+	return (dest);
+}
