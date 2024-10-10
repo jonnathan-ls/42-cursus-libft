ce60ccd863c2ff6465ecd768c8eff0356574078a
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2024-10-10 20:24:02 -0300

    feat: adds implementation for strlcpy function

---
+++ srcs/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 20:00:15 by jlacerda          #+#    #+#             */
+/*   Updated: 2024/10/10 20:20:03 by jlacerda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned long	ft_strlcpy(char *dst, const char *src, unsigned long size)
+{
+	unsigned long	i;
+	unsigned long	src_len;
+
+	i = 0;
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	if (size > 0)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (src_len);
+}
