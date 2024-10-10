375b9efb0b8605a4faaf7fa2dd55abf3d0010e5e
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2024-10-10 20:48:52 -0300

    feat: adds implementation for strnstr function

---
+++ srcs/ft_strnstr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 20:00:15 by jlacerda          #+#    #+#             */
+/*   Updated: 2024/10/10 20:43:42 by jlacerda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *big, const char *little, unsigned long len)
+{
+	unsigned long	i;
+	unsigned long	j;
+
+	i = 0;
+	if (!*little)
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] == little[j] && little[j] && i + j < len)
+			j++;
+		if (!little[j])
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (0);
+}
