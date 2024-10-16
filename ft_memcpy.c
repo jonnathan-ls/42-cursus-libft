de1f4d36c7240080ba310d5da2cb5e848f396c3b
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2024-10-16 20:03:20 -0300

    fix: adds implementation when strings are null

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,15 +6,20 @@
 /*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 19:56:49 by jlacerda          #+#    #+#             */
-/*   Updated: 2024/10/08 20:46:33 by jlacerda         ###   ########.fr       */
+/*   Updated: 2024/10/15 20:46:03 by jlacerda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include	"libft.h"
 
 void *ft_memcpy(void *dest, const void *src, unsigned long n)
 {
 	unsigned long	i;
 	char			*ptr_dest;
 	char			*ptr_src;
+
+	if (!dest && !src)
+		return (NULL);
 
 	i = 0;
 	ptr_dest = (char *)dest;
